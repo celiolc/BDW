4f187ea42041e7288e83ef38cbe689a1a5858c74
IBM899-PE029WR2\celiolc <celioldc@gmail.com>
2018-07-18 08:56:00 -0300

    background changes

--- js/custom.js
+++ js/custom.js
@@ -1,60 +1,60 @@
-
 // ISOTOPE FILTER
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
 
-  if ( $('.iso-box-wrapper').length > 0 ) { 
+    if ($('.iso-box-wrapper').length > 0) {
 
-      var $container  = $('.iso-box-wrapper'), 
-        $imgs     = $('.iso-box img');
+        var $container = $('.iso-box-wrapper'),
+            $imgs = $('.iso-box img');
 
-      $container.imagesLoaded(function () {
+        $container.imagesLoaded(function() {
 
-        $container.isotope({
-        layoutMode: 'fitRows',
-        itemSelector: '.iso-box'
+            $container.isotope({
+                layoutMode: 'fitRows',
+                itemSelector: '.iso-box'
+            });
+
+            $imgs.load(function() {
+                $container.isotope('reLayout');
+            })
+
         });
 
-        $imgs.load(function(){
-          $container.isotope('reLayout');
-        })
+        //filter items on button click
 
-      });
+        $('.filter-wrapper li a').click(function() {
 
-      //filter items on button click
+            var $this = $(this),
+                filterValue = $this.attr('data-filter');
 
-      $('.filter-wrapper li a').click(function(){
+            $container.isotope({
+                filter: filterValue,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false,
+                }
+            });
 
-          var $this = $(this), filterValue = $this.attr('data-filter');
+            // don't proceed if already selected 
 
-      $container.isotope({ 
-        filter: filterValue,
-        animationOptions: { 
-            duration: 750, 
-            easing: 'linear', 
-            queue: false, 
-        }                
-      });             
+            if ($this.hasClass('selected')) {
+                return false;
+            }
 
-      // don't proceed if already selected 
+            var filter_wrapper = $this.closest('.filter-wrapper');
+            filter_wrapper.find('.selected').removeClass('selected');
+            $this.addClass('selected');
 
-      if ( $this.hasClass('selected') ) { 
-        return false; 
-      }
+            return false;
+        });
 
-      var filter_wrapper = $this.closest('.filter-wrapper');
-      filter_wrapper.find('.selected').removeClass('selected');
-      $this.addClass('selected');
-
-        return false;
-      }); 
-
-  }
+    }
 
 });
 
 
 // PRELOADER JS
-$(window).load(function(){
+$(window).load(function() {
     $('.preloader').fadeOut(1000); // set duration in brackets    
 });
 
@@ -71,37 +71,37 @@ $(window).scroll(function() {
 
 /* HTML document is loaded. DOM is ready. 
 -------------------------------------------*/
-$(function(){
+$(function() {
 
-  // ------- WOW ANIMATED ------ //
-  wow = new WOW(
-  {
-    mobile: false
-  });
-  wow.init();
+    // ------- WOW ANIMATED ------ //
+    wow = new WOW({
+        mobile: false
+    });
+    wow.init();
 
 
-  // HIDE MOBILE MENU AFTER CLIKING ON A LINK
-  $('.navbar-collapse a').click(function(){
+    // HIDE MOBILE MENU AFTER CLIKING ON A LINK
+    $('.navbar-collapse a').click(function() {
         $(".navbar-collapse").collapse('hide');
     });
 
 
-  // NIVO LIGHTBOX
-  $('.iso-box-section a').nivoLightbox({
+    // NIVO LIGHTBOX
+    $('.iso-box-section a').nivoLightbox({
         effect: 'fadeScale',
     });
 
 
-  // HOME BACKGROUND SLIDESHOW
-  $(function(){
-    jQuery(document).ready(function() {
-    $('#home').backstretch([
-       "images/ibm-watson.jpg", 
-       "images/ibm-watson.jpg",
-        ],  {duration: 2000, fade: 750});
-    });
-  })
+    // HOME BACKGROUND SLIDESHOW
+    $(function() {
+        jQuery(document).ready(function() {
+            $('#home').backstretch([
+                "https://github.com/celiolc/BDW/blob/master/images/ibm-watson.jpg",
+                "https://github.com/celiolc/BDW/blob/master/images/ibm-watson.jpg",
+                /*"images/ibm-watson.jpg", 
+                "images/ibm-watson.jpg",*/
+            ], { duration: 2000, fade: 750 });
+        });
+    })
 
 });
-
